9a82f302597f845aaa80b7d56c0ecd65c4ba31cf
Stelly <stellyita@gmail.com>
2024-09-20 11:01:23 +0200

    Add json deserialization tests

--- src/test/java/com/order_manager/order_manager/MenuItemJsonTests.java
+++ src/test/java/com/order_manager/order_manager/MenuItemJsonTests.java
@@ -2,11 +2,11 @@ package com.order_manager.order_manager;
 
 import java.io.IOException;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.json.JacksonTester;
-import static org.assertj.core.api.Assertions.assertThat;
 import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.boot.test.json.JacksonTester;
 
 @JsonTest
 public class MenuItemJsonTests {
@@ -29,5 +29,25 @@ public class MenuItemJsonTests {
         assertThat(json.write(testItem)).extractingJsonPathNumberValue("$.price").isEqualTo(18.79);
         assertThat(json.write(testItem)).hasJsonPathBooleanValue("$.available");
         assertThat(json.write(testItem)).extractingJsonPathBooleanValue("$.available").isEqualTo(true);
+    }
+
+    @Test
+    void jsonDeserializationTest() throws IOException {
+        String expected = """
+                {
+                    "dish_id": 1,
+                    "dish_name": "Antipasto di terra",
+                    "category": "starter",
+                    "price": 18.79,
+                    "available": true
+                }
+                """;
+
+        assertThat(json.parse(expected)).isEqualTo(new MenuItem(1,"Antipasto di terra","starter",18.79f,true));
+        assertThat(json.parseObject(expected).dish_id()).isEqualTo(1);
+        assertThat(json.parseObject(expected).dish_name()).isEqualTo("Antipasto di terra");
+        assertThat(json.parseObject(expected).category()).isEqualTo("starter");
+        assertThat(json.parseObject(expected).price()).isEqualTo(18.79f);
+        assertThat(json.parseObject(expected).available()).isEqualTo(true);
     }
 }
