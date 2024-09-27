ee0dca4bcd376b560ef9e356714e94d8cd46cbe8
Stelly <stellyita@gmail.com>
2024-09-27 16:36:36 +0200

    Adjust price position in grid item

--- frontend/src/components/MenuItem.js
+++ frontend/src/components/MenuItem.js
@@ -16,6 +16,7 @@ function MenuItem({ name, image, price, available }) {
         <div className="menu-item">
             <img src={image} alt={name + " thumbnail"} className="item-image"></img>
             <div>{name}</div>
+            <br/>
             <div className="price">
                 <small>{available ? priceString : "Not available"}</small>
             </div>
