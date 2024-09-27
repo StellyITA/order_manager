94bd8c3263b0920cc6d9a7eac8403660e3fb0002
Stelly <stellyita@gmail.com>
2024-09-27 20:43:19 +0200

    Add categories tabs with some styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,67 +1,82 @@
 import './App.css';
 import MenuItem from './components/MenuItem';
 import { useState, useEffect } from 'react';
-import axios from 'axios';
+import { menuItemsData, categoriesData } from './Data';
 
 function App() {
 
   const [data, setData] = useState([]);
   const [categories, setCategories] = useState([]);
+  const [currentCategory, setCurrentCategory] = useState(0);
+
+  async function getMenu() {
+    const menuData = await menuItemsData();
+
+    menuData.forEach(item => {
+      if (item.dish_image != null) {
+        item.dish_image = item.dish_image.split(",").map(e => parseInt(e) >= 0 ? parseInt(e) : parseInt(e) + 256);
+        item.dish_image = "data:img/png;base64," + btoa(String.fromCharCode.apply(null, item.dish_image));
+      }
+    });
+    setData(menuData);
+  }
+
+  async function getCategories() {
+    const categories = await categoriesData();
+    setCategories(categories);
+  }
 
   useEffect(() => {
-
-    // GET menu items
-    axios.get("http://localhost:8080/menu")
-         .then((response) => {
-
-          let jsonRes = response.data;
-
-          jsonRes.forEach(item => {
-            if (item.dish_image != null) {
-              item.dish_image = item.dish_image.split(",").map(e => parseInt(e) >= 0 ? parseInt(e) : parseInt(e) + 256);
-              item.dish_image = "data:img/png;base64," + btoa(String.fromCharCode.apply(null, item.dish_image));
-            }
-          });
-          setData(jsonRes);
-         })
-         .catch(err => {
-          console.log(err);
-         })
-
-    // GET categories
-    axios.get("http://localhost:8080/menu/categories")
-         .then((response) => {
-          setCategories(response.data);
-         })
-         .catch(err => {
-          console.log(err);
-         })
+    getMenu();
+    getCategories();
   },[]);
+
+  function handleCategoryClick(event, category) {
+    const prev = document.getElementById('active');
+    prev.setAttribute("id",null);
+    prev.setAttribute("class","unactive");
+    event.target.id = 'active';
+    event.target.className = "active";
+    setCurrentCategory(category);
+  }
 
   return (
     <div className="App">
       <header className="App-header">
         Header
       </header>
-      <div>
-        <button>Tutte le categorie</button>
+      <div className='categories'>
+        <button 
+          id='active'
+          className='active'
+          key='All0'
+          onClick={(e) => handleCategoryClick(e,0)}
+        >Tutte le categorie</button>
         {categories.map(el => {
           return (
-            <button>{el.category_name}</button>
+            <button 
+              className='unactive'
+              key={el["category_name"] + el["category_id"]}
+              onClick={(e) => handleCategoryClick(e,el["category_id"])}
+            >{el["category_name"]}</button>
           )
         })}
       </div>
       <div className='display-menu'>
-      {data.map(el => {
-          return (
-            <MenuItem
-              name={el["dish_name"]}
-              image={el["dish_image"]}
-              price={el["price"]}
-              available={el["available"]}
-              key={el["dish_id"]}
-            />
-          )
+        {data.map(el => {
+            if (currentCategory === 0 || el["category"] === currentCategory) {
+              return (
+                <MenuItem
+                  name={el["dish_name"]}
+                  image={el["dish_image"]}
+                  price={el["price"]}
+                  available={el["available"]}
+                  key={el["dish_id"]}
+                />
+              )
+            } else {
+              return null;
+            }
         })}
       </div>
     </div>
