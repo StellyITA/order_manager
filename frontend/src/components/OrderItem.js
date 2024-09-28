e5c51cdcaaa245c583142204eba2e84504b79f83
Stelly <stellyita@gmail.com>
2024-09-28 20:15:16 +0200

    Style order item

--- frontend/src/components/OrderItem.js
+++ frontend/src/components/OrderItem.js
@@ -1,10 +1,15 @@
+import "./OrderItem.css"
 import { formatPriceString } from "../UtilityFunctions";
 
 export default function OrderItem({ id, quantity, name, price }) {
     
-    const priceString = formatPriceString(price);
+    const priceString = formatPriceString(price * quantity);
 
     return (
-        <p id={id}><span>{quantity}</span> <span>{name}</span> <span>{priceString}</span></p>
+        <p id={id} className="order-item">
+            <span className="order-item-quantity">{quantity}</span> 
+            <span className="order-item-name">{name}</span> 
+            <span className="order-item-price">{priceString}</span>
+        </p>
     )
 }
